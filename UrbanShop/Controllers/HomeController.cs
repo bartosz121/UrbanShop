d2f284eb32825a1b0d49a62db6efe6571b085120
bartosz121 <bmagiera121@gmail.com>
2022-01-11 18:22:53 +0100

    Update home controller

--- UrbanShop/Controllers/HomeController.cs
+++ UrbanShop/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using UrbanShop.Data;
 using UrbanShop.Models;
 
 namespace UrbanShop.Controllers
@@ -12,13 +14,23 @@ namespace UrbanShop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ShopContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ShopContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.Include(p => p.ProductImages.Take(1)).Take(8).ToListAsync();
+
+            return View(products);
+        }
+
+        // TODO authentication
+        public IActionResult AdminPanel()
         {
             return View();
         }
