42a40472b4940871a5aebcec8c1c25f53ac04c1e
bartosz121 <bmagiera121@gmail.com>
2022-01-24 23:10:15 +0100

    Add `AspRouteBtn` view component

---
+++ UrbanShop/ViewComponents/AspRouteBtn.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrbanShop.ViewComponents
+{
+    public class AspRouteBtn : ViewComponent
+    {
+        public class AspRouteBtnContext{
+            public string controller { get; set; }
+            public string action { get; set; }
+            public string wrapperClass { get; set; }
+            public string btnText { get; set; }
+        }
+
+        public IViewComponentResult Invoke(string controller, string action, string wrapClass, string btnText)
+        {
+            var context = new AspRouteBtnContext();
+            context.controller = controller;
+            context.action = action;
+            context.wrapperClass = wrapClass;
+            context.btnText = btnText;
+
+            return View("Default", context);
+        }
+    }
+}
