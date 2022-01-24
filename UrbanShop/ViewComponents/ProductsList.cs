67cb3c160f27336bea66e4afc2379308341fe71f
bartosz121 <bmagiera121@gmail.com>
2022-01-24 23:10:38 +0100

    Add `ProductsList` view component

---
+++ UrbanShop/ViewComponents/ProductsList.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrbanShop.Data;
+using UrbanShop.Models;
+
+namespace UrbanShop.ViewComponents
+{
+    public class ProductsList : ViewComponent
+    {
+
+        public class ProductsListContext
+        {
+            public Category category { get; set; }
+            public bool seeMoreVisible { get; set; }
+        }
+
+
+        public IViewComponentResult Invoke(Category category, bool seeMore)
+        {
+            var context = new ProductsListContext();
+            context.category = category;
+            context.seeMoreVisible = seeMore;
+
+            return View("Default", context);
+        }
+    }
+}
