4ad329bc07ade9228e7ee2030863de722223b56a
bartosz121 <bmagiera121@gmail.com>
2022-01-25 23:58:53 +0100

    Add `Category` model

---
+++ UrbanShop/Models/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrbanShop.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string DisplayName { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        [Required]
+        public bool IsVisible { get; set; }
+    }
+}
