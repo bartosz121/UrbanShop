3a8970b8ca8c88cb2e38a9eaf91248a341845ef5
bartosz121 <bmagiera121@gmail.com>
2022-01-31 21:45:07 +0100

    Add `Product` model

---
+++ UrbanShop/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrbanShop.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(19, 2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public List<ProductImage> ProductImages { get; set; } = new List<ProductImage>();
+
+        [Required]
+        public bool IsVisible { get; set; }
+    }
+
+}
