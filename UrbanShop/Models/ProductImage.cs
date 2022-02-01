510a63fd3d1a58c6aa3a0bda9a8080f687304b3d
bartosz121 <bmagiera121@gmail.com>
2022-02-01 23:16:13 +0100

    Add `ProductImage` model

---
+++ UrbanShop/Models/ProductImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrbanShop.Models
+{
+    public class ProductImage
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+    }
+}
