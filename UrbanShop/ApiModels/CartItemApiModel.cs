243fd441289aef4f862cecf70ae20cd3a6e2d245
bartosz121 <bmagiera121@gmail.com>
2022-02-11 21:20:40 +0100

    Add `CartItem` api model

---
+++ UrbanShop/ApiModels/CartItemApiModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrbanShop.ApiModels
+{
+    public class CartItemApiModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductThumbnail { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
