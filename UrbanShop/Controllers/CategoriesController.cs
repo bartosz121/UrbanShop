7ef6c9bd71d8a06395c103370606915974c4e5e6
bartosz121 <bmagiera121@gmail.com>
2022-02-11 21:24:21 +0100

    Add authorization

--- UrbanShop/Controllers/CategoriesController.cs
+++ UrbanShop/Controllers/CategoriesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using UrbanShop.Models;
 
 namespace UrbanShop.Controllers
 {
+    [Authorize(Roles = "Admin")]
     [Route("/admin/[controller]/[action]")]
     public class CategoriesController : Controller
     {
