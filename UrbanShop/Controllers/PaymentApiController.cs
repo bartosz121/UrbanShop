71813a4529a32294fe261aa684b22adb8a915fce
bartosz121 <bmagiera121@gmail.com>
2022-02-15 23:27:43 +0100

    Add `PaymentApi` controller

---
+++ UrbanShop/Controllers/PaymentApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Stripe;
+
+namespace UrbanShop.Controllers
+{
+    [Route("api/create-payment-intent")]
+    [ApiController]
+    public class PaymentIntentApiController : Controller
+    {
+        [HttpPost]
+        public ActionResult Create(PaymentIntentCreateRequest request)
+        {
+            long orderPrice = Convert.ToInt64(Convert.ToDecimal(request.totalOrderAmount));
+            var paymentIntentService = new PaymentIntentService();
+            var paymentIntent = paymentIntentService.Create(new PaymentIntentCreateOptions
+            {
+                Amount = orderPrice,
+                Currency = "eur",
+                AutomaticPaymentMethods = new PaymentIntentAutomaticPaymentMethodsOptions
+                {
+                    Enabled = true,
+                },
+            });
+
+            return Json(new { clientSecret = paymentIntent.ClientSecret });
+        }
+
+        public class PaymentIntentCreateRequest
+        {
+            [JsonProperty("totalOrderAmount")]
+            public string totalOrderAmount { get; set; }
+
+        }
+    }
+}
