8be75c6b2bab9a7ad329b2a85c6ef79def1da030
bartosz121 <bmagiera121@gmail.com>
2022-02-17 20:43:24 +0100

    Add `ProductPageViewModel`

---
+++ UrbanShop/ViewModels/ProductPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrbanShop.Models;
+
+namespace UrbanShop.ViewModels
+{
+    public class ProductPageViewModel
+    {
+        public Product product { get; set; }
+        public List<Product> relatedProducts { get; set; }
+    }
+}
