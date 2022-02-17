335948ec3ed01265eb48f8ee8e4b0fa3adfc0492
bartosz121 <bmagiera121@gmail.com>
2022-02-17 20:41:02 +0100

    Change the way price is calculated
    - Instead of converting price sent in POST request, now client sends item list; price is calculated from its `totalPrice` field and multiplied by 100 because stripe needs value in cents

--- UrbanShop/Controllers/PaymentApiController.cs
+++ UrbanShop/Controllers/PaymentApiController.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UrbanShop.ApiModels;
 using Newtonsoft.Json;
 using Stripe;
+using UrbanShop.Data;
 
 namespace UrbanShop.Controllers
 {
@@ -12,10 +14,17 @@ namespace UrbanShop.Controllers
     [ApiController]
     public class PaymentIntentApiController : Controller
     {
-        [HttpPost]
-        public ActionResult Create(PaymentIntentCreateRequest request)
+        private readonly ShopContext _context;
+
+        public PaymentIntentApiController(ShopContext context)
         {
-            long orderPrice = Convert.ToInt64(Convert.ToDecimal(request.totalOrderAmount));
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(PaymentIntentCreateRequest request)
+        {
+            var orderPrice = await GetOrderPrice(request.items);
             var paymentIntentService = new PaymentIntentService();
             var paymentIntent = paymentIntentService.Create(new PaymentIntentCreateOptions
             {
@@ -30,10 +39,29 @@ namespace UrbanShop.Controllers
             return Json(new { clientSecret = paymentIntent.ClientSecret });
         }
 
+        private async Task<long> GetOrderPrice(List<CartItemApiModel> items)
+        {
+            long totalPrice = 0;
+            foreach (var item in items)
+            {
+                totalPrice += Convert.ToInt64(Decimal.Multiply(item.TotalPrice, 100));
+                var cartItem = await _context.CartItem.FindAsync(item.Id);
+                if (cartItem != null)
+                {
+                    _context.CartItem.Remove(cartItem);
+                }
+            }
+
+            await _context.SaveChangesAsync();
+
+            return totalPrice;
+
+        }
+
         public class PaymentIntentCreateRequest
         {
-            [JsonProperty("totalOrderAmount")]
-            public string totalOrderAmount { get; set; }
+            [JsonProperty("items")]
+            public List<CartItemApiModel> items { get; set; }
 
         }
     }
