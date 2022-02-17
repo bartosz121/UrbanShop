1dbf617756bdc2f9a63d64b451d32ce743a2eacf
bartosz121 <bmagiera121@gmail.com>
2022-02-17 20:42:56 +0100

    Add `ProductImageViewModel`

---
+++ UrbanShop/ViewModels/ProductImageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrbanShop.Models;
+
+namespace UrbanShop.ViewModels
+{
+    public class ProductImageViewModel
+    {
+        public List<IFormFile> Images { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
